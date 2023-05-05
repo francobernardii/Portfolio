7597dfcb8279a90ec1666ae66734a78dead66e0f
Franco Bernardi <francobernardi134@gmail.com>
2023-05-05 14:43:41 -0300

    Se modifica la URL de los iconos

--- src/Components/Header/config.js
+++ src/Components/Header/config.js
@@ -4,25 +4,25 @@ const navBarButtons = [
     {
         text: 'Home',
         url: '/',
-        iconURL: 'src/assets/Header/NavBar/home.svg',
+        iconURL: '/src/assets/Header/NavBar/home.svg',
         iconALT: 'Home Button'
     },
     {
         text: 'About Me',
         url: '/AboutMe',
-        iconURL: 'src/assets/Header/NavBar/aboutMe.svg',
+        iconURL: '/src/assets/Header/NavBar/aboutMe.svg',
         iconALT: 'About Me Button'
     },
     {
         text: 'Projects',
         url: '/Projects',
-        iconURL: 'src/assets/Header/NavBar/projects.svg',
+        iconURL: '/src/assets/Header/NavBar/projects.svg',
         iconALT: 'Projects Button'
     },
     {
         text: 'Contact Me',
         url: '/Contact',
-        iconURL: 'src/assets/Header/NavBar/contactMe.svg',
+        iconURL: '/src/assets/Header/NavBar/contactMe.svg',
         iconALT: 'Contact Me Button'
     }
 ]
